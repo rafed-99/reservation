ca69f6a4b15f1e72282180b5a8c1a6129d01d4bc
rafed-99 <73440128+rafed-99@users.noreply.github.com>
2022-10-03 23:10:09 +0100

    update

--- models/reservation.js
+++ models/reservation.js
@@ -4,9 +4,9 @@ const reservationSchema = new mongoose.Schema({
 
     dateAller : { type : Date , required : true },
     dateRetour : { type : Date , required : true },
-    nbrPersonnes : { type : Number , required : true },
+    nbrPersonnes : { type : Number , required : true },  // n5ammem nbadelha btari9a o5ra 
     paysDepart : { type : mongoose.SchemaTypes.ObjectId , ref : "PaysDepart"},  //relation one to one 
-    destination : { type : mongoose.SchemaTypes.ObjectId , ref : "Destination"},
+    destination : { type : mongoose.SchemaTypes.ObjectId , ref : "Destination"},  //c'est le meme de pays d'arrive
     moyenTransport : { type : mongoose.SchemaTypes.ObjectId , ref : "MoyenTransport"},
     allogement : { type : mongoose.SchemaTypes.ObjectId , ref : "Allogement"},
     prixTotal : { type : Number , required : true }
