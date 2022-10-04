ba5290c1feb441afa10df0002f281934a11d58f6
rafed-99 <73440128+rafed-99@users.noreply.github.com>
2022-10-04 15:22:39 +0100

    update models

---
+++ models/informationDestination.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const informationDestinationSchema = new mongoose.Schema({
+
+    capitale : { Type : String , required : true , unique : true },
+    population : { Type : Number , required : true , unique : true },
+    devise : { Type : String , required : true }
+
+});
+const informationDestination = mongoose.model("informationDestination",informationDestinationSchema);
+module.exports = informationDestination;
